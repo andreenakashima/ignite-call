de2307a071332cb287dd4cfe7ac1c305128bc598
andreenakashima <andreenakashima@gmail.com>
2023-01-08 15:01:46 -0300

    Rotas autenticadas no Next

--- src/pages/register/time-intervals/index.page.tsx
+++ src/pages/register/time-intervals/index.page.tsx
@@ -21,6 +21,7 @@ import {
   IntervalItem,
 } from './styles'
 import { convertTimeStringInMinutes } from '../../../utils/convert-time-string-in-minutes'
+import { api } from '../../../lib/axios'
 
 const timeIntervalsFormSchema = z.object({
   intervals: z
@@ -95,8 +96,9 @@ export default function TimeIntervals() {
   const intervals = watch('intervals')
 
   async function handleSetTimeIntervals(data: any) {
-    const formData = data as TimeIntervalsFormOutput
-    console.log(formData)
+    const { intervals } = data as TimeIntervalsFormOutput
+
+    await api.post('/users/time-intervals', { intervals })
   }
 
   return (
